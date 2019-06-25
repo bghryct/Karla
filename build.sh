d4ce019cc9cdb5f02606b6bd866dee90a414571e
Mirko Velimirovic <bghryct@gmail.com>
2019-06-25 19:07:53 -0400

    Changed naming conventions

--- build.sh
+++ build.sh
@@ -8,8 +8,6 @@
 # -------------------------------------------------------------------
 # Update the following as needed ------------------------------------
 set -e
-# source venv/bin/activate
-# echo "I should have activated a virtual environment now"
 
 
 echo "Generating Static fonts"
@@ -18,9 +16,9 @@ mkdir -p ./fonts/ttf
 mkdir -p ./fonts/variable
 
 echo "Made font directories"
-fontmake -g sources/Karla-Roman.glyphs -i -o ttf --output-dir ./fonts/ttf/
+fontmake -g sources/Karla-Roman.glyphs -i --round-instances -o ttf --output-dir ./fonts/ttf/ 
 echo "Made Roman ttfs"
-fontmake -g sources/Karla-Italic.glyphs -i -o ttf --output-dir ./fonts/ttf/
+fontmake -g sources/Karla-Italic.glyphs -i --round-instances -o ttf --output-dir ./fonts/ttf/
 echo "Made Italic ttfs"
 
 # fontmake -g sources/Karla-Roman.glyphs -i -o otf --output-dir ./fonts/otf/
@@ -30,8 +28,8 @@ echo "Made Italic ttfs"
 
 echo "Generating VFs"
 # mkdir -p ./fonts/variable
-fontmake -g sources/Karla-Roman.glyphs -o variable --output-path ./fonts/variable/Karla-Roman-VF.ttf
-fontmake -g sources/Karla-Italic.glyphs -o variable --output-path ./fonts/variable/Karla-Italic-VF.ttf
+fontmake -g sources/Karla-Roman.glyphs -o variable --round-instances --output-path ./fonts/variable/Karla\[wght\].ttf
+fontmake -g sources/Karla-Italic.glyphs -o variable --round-instances --output-path ./fonts/variable/Karla-Italic\[wght\].ttf
 
 echo "Removing Build UFOS"
 
@@ -81,89 +79,3 @@ echo "Post processing complete"
 
 cp /Users/mirkovelimirovic/Documents/GitHub/Karla/fonts/OFL.txt /Users/mirkovelimirovic/Documents/GitHub/Karla/fonts/ttf/OFL.txt
 cp /Users/mirkovelimirovic/Documents/GitHub/Karla/fonts/OFL.txt /Users/mirkovelimirovic/Documents/GitHub/Karla/fonts/variable/OFL.txt
-
-# echo "fixing name-ids"
-# gftools fix-nameids $vfs;
-# for vf in $vfs 
-# do 
-# 	mv "$vf.fix" $vf;
-# done
-
-# echo "fixed name-ids"
-# # cd ..
-
-# # # ============================================================================
-# # # Autohinting ================================================================
-
-# statics=$(ls ./fonts/ttf/*.ttf)
-# echo hello
-# for file in $statics; do 
-#     echo "fix DSIG in " ${file}
-#     gftools fix-dsig --autofix ${file}
-
-
-#     echo "TTFautohint " ${file}
-#     # autohint with detailed info
-#     hintedFile=${file/".ttf"/"-hinted.ttf"}
-#     ttfautohint -I ${file} ${hintedFile} 
-#     cp ${hintedFile} ${file}
-#     rm -rf ${hintedFile}
-# done
-
-# # # ============================================================================
-# # # Fix Hinting ================================================================
-
-# statics=$(ls ./fonts/ttf/*.ttf)
-# echo "Hi Mirko I am trying to fix the hinting in the statics"
-# for file in $statics; do 
-# 	echo "fix hinting in " ${file}
-# 	# fix hinting 
-#     gftools fix-hinting ${file} 
-#  done
-
-#   ###Remove Broken ttfs ###
-# 	echo "rm rfing ttfs"
-# 	rm -rf fonts/ttf/*.ttf
-# 	echo "ttfs removed"
-
-
-#   # Rename all *.ttf.fix to *.ttf                                                                 
-# for f in ./fonts/ttf/*.ttf.fix; do 
-#     mv -- "$f" "${f%.ttf.fix}.ttf"     
-# done
-
-
-
-# # Build woff2 fonts ==========================================================
-
-# # requires https://github.com/bramstein/homebrew-webfonttools
-
-# rm -rf fonts/woff2
-
-# ttfs=$(ls ./fonts/*/*.ttf)
-# for ttf in $ttfs; do
-#     woff2_compress $ttf
-# done
-
-# mkdir -p fonts/woff2
-# woff2s=$(ls ./fonts/*/*.woff2)
-# for woff2 in $woff2s; do
-#     mv $woff2 fonts/woff2/$(basename $woff2)
-# done
-# # # ============================================================================
-# # # Build woff fonts ==========================================================
-
-# # # requires https://github.com/bramstein/homebrew-webfonttools
-
-# # rm -rf fonts/woff
-
-# ttfs=$(ls ./fonts/*/*.ttf)
-# for ttf in $ttfs; do
-#     sfnt2woff-zopfli $ttf
-# done
-
-# mkdir -p fonts/woff
-# woffs=$(ls ./fonts/*/*.woff)
-# for woff in $woffs; do
-#     mv $woff fonts/woff/$(basename $woff)
-# done
